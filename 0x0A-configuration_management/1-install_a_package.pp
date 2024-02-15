0b54235c977ce13af3d8a0172f1a07b9f9fdfa5f
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-15 02:43:50 +0000

    installed werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -12,8 +12,15 @@ package { 'python3-pip':
   require =>  Package['python3'],
 }
 
+
+exec { 'install_werkzeug':
+  command =>  'pip3 install Werkzeug==2.1.1',
+  unless  =>  'pip3 freeze | grep Werkzeug==2.1.1',
+  require =>   Package['python3-pip'],
+}
+
 package { 'flask':
   ensure   =>  '2.1.0',
-  require  =>  Package['python3-pip'],
+  require  =>  Exec['install_werkzeug'],
   provider =>  'pip3',
 }
