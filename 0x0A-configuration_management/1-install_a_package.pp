3314789918e2fecee345c448fbec2b2f3bf03301
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-15 02:13:13 +0000

    installed pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,6 +2,12 @@
 # Requirements
 # Flask - 2.1.0
 
+exec { 'install_pip3':
+  command =>  'apt-get install python3-pip',
+}
+
 package { 'flask':
-  ensure =>  '2.1.0',
+  ensure   =>  '2.1.0',
+  require  =>  'exec[install_pip3]',
+  provider =>  'pip3',
 }
