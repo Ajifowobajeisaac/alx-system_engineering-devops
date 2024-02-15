5cfb7005131d23c98207eb7e44601cdd9a32c997
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-15 01:51:25 +0000

    installed python manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,13 @@
 # Requirements
 # Flask - 2.1.0
 
+package { 'python3':
+  ensure  => 'installed',
+}
+
 package { 'pip3':
   ensure   =>  '2.1.0',
+  require  =>  'Package[python3]',
   name     =>  'flask',
   provider =>  'pip3',
 }
