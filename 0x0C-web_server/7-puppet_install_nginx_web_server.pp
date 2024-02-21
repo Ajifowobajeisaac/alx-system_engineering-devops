a373077c93729420287034fbf968694945b1d538
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-02-21 18:10:22 +0000

    modified puppet nginx config file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,62 +1,55 @@
-# Pupeet manifest to intall and confugure nginx
-# Requirements
-# root page serves 'Hello World!'
-# Redirection of /redirect_me
-
-# 7-puppet_install_nginx_web_server.pp
-
-
+# Ensure Nginx is installed
 package { 'nginx':
   ensure => installed,
 }
 
+# Ensure Nginx service is running and enabled
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
+  ensure => running,
+  enable => true,
   require => Package['nginx'],
 }
 
+# Allow HTTP through the firewall
 exec { 'allow nginx http through ufw':
   command => '/usr/sbin/ufw allow http',
   unless  => '/usr/sbin/ufw status | grep "Nginx HTTP"',
   require => Package['nginx'],
 }
 
-file { '/var/www/html/index.html':
+# Ensure the document root directory exists
+file { '/var/www/chefzeek.tech/html':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
+  require => Package['nginx'],
+}
+
+# Create a simple HTML file
+file { '/var/www/chefzeek.tech/html/index.html':
   ensure  => file,
   content => '<p>Hello World</p>',
-  require => Package['nginx'],
+  require => File['/var/www/chefzeek.tech/html'],
 }
 
-file { '/var/www/html':
-  ensure  => directory,
-  owner   => 'www-data',
-  group   => 'www-data',
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
+# Configure Nginx server block
+file { '/etc/nginx/sites-available/chefzeek_server':
   ensure  => file,
-  content => @(EOF),
-  server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name chefzeek.tech;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location ~ ^/redirect_me/?$ {
-        return 301 /index.html;
-    }
-  }
-  | EOF
-  ,
+  content => template('path/to/your/template.erb'),
   require => Package['nginx'],
   notify  => Service['nginx'],
+}
+
+# Enable the Nginx server block
+file { '/etc/nginx/sites-enabled/chefzeek_server':
+  ensure => link,
+  target => '/etc/nginx/sites-available/chefzeek_server',
+  require => File['/etc/nginx/sites-available/chefzeek_server'],
+  notify  => Service['nginx'],
+}
+
+# Remove the default Nginx server block
+file { '/etc/nginx/sites-enabled/default':
+  ensure => absent,
+  notify => Service['nginx'],
 }
