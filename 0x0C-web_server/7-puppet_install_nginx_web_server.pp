68895d1e0315dd37937352c65cf9abded4414be0
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2024-03-22 11:50:57 +0000

    puppet web server installation script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,55 +1,6 @@
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure => installed,
-}
+# Installs a Nginx server
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure => running,
-  enable => true,
-  require => Package['nginx'],
-}
-
-# Allow HTTP through the firewall
-exec { 'allow nginx http through ufw':
-  command => '/usr/sbin/ufw allow http',
-  unless  => '/usr/sbin/ufw status | grep "Nginx HTTP"',
-  require => Package['nginx'],
-}
-
-# Ensure the document root directory exists
-file { '/var/www/chefzeek.tech/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  require => Package['nginx'],
-}
-
-# Create a simple HTML file
-file { '/var/www/chefzeek.tech/html/index.html':
-  ensure  => file,
-  content => '<p>Hello World</p>',
-  require => File['/var/www/chefzeek.tech/html'],
-}
-
-# Configure Nginx server block
-file { '/etc/nginx/sites-available/chefzeek_server':
-  ensure  => file,
-  content => template('path/to/your/template.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Enable the Nginx server block
-file { '/etc/nginx/sites-enabled/chefzeek_server':
-  ensure => link,
-  target => '/etc/nginx/sites-available/chefzeek_server',
-  require => File['/etc/nginx/sites-available/chefzeek_server'],
-  notify  => Service['nginx'],
-}
-
-# Remove the default Nginx server block
-file { '/etc/nginx/sites-enabled/default':
-  ensure => absent,
-  notify => Service['nginx'],
-}
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}}
